cef4cf60a92369a569d29ef057909aef90b0cb37
Sadov74 <143268720+Sadov74@users.noreply.github.com>
2025-09-05 05:43:43 +0300

    Update HelloWorld.java

--- 01_basics/HelloWorld.java
+++ 01_basics/HelloWorld.java
@@ -1,6 +1,14 @@
-public class HelloWorld{
+/**
+ * This class prints "Hello, World!" to the console.
+ */
+public class HelloWorld {
 
-    public static void main(String[] args){
+    /**
+     * Entry point of the program.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        // Prints a message to the console
         System.out.println("Hello, World!");
-    }
-}
+    } // End of the main method
+} // End of public class HelloWorld
